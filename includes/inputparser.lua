8ca9fc02ed55f5eb683272d0161f2d6e54d3cba5
iggyvolz <iggyvolz@aim.com>
2014-10-24 18:49:21 +0000

    Add input parser for iggyvolz/iMagine#19

---
+++ includes/inputparser.lua
@@ -0,0 +1,49 @@
+-- Temporary default subject - reprogram later
+defaultSubject="tony"
+function inputParser (text)
+   local otext=text
+   local stage=1
+   local subject=""
+   local action=""
+   local params={}
+   local continue=0
+   local char=""
+   if not text:find(".",1,1) then
+      text="i."..text
+   end
+   for i=1,#text do
+      continue=0
+      char=text:sub(i,i)
+      if stage==1 then
+         if char=="." then
+            stage=2
+            continue=1
+            if subject=="i" then
+               subject=defaultSubject
+            end
+         else
+            subject=subject..char
+         end
+      end
+      if stage==2 and continue~=1 then
+         if char=="(" then
+            stage=3
+            table.insert(params,"")
+            continue=1
+         else
+            action=action..char
+         end
+      end
+      if stage==3 and continue~=1 then
+         if char==")" then
+            break
+         end
+         if char=="," then
+            table.insert(params,"")
+         else
+            params[#params]=params[#params]..char
+         end
+      end
+   end
+   return text,subject,action,unpack(params)
+end
