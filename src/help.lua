c0fce3f30b7a4da945b40f8054bf8849b28787de
iggyvolz <iggyvolz@aim.com>
2014-10-25 19:54:57 -0400

    Add help

---
+++ src/help.lua
@@ -0,0 +1,8 @@
+if _ == nil then
+  function _(str)
+    return str
+  end
+end
+function help()
+  return {_("Please see http://iggyvolz.github.io/iMagine/documentation.html for help"),_("NOTE: still in development so this documentation is likely irrelevant")}
+end
---
+++ src/help_test.lua
@@ -0,0 +1,29 @@
+if not describe then
+  names={}
+  function describe(name,fn)
+    table.insert(names,name)
+    local index=#names
+    fn()
+    table.remove(names,index)
+  end
+  function it(name,fn)
+    table.insert(names,name)
+    local index=#names
+    print(table.concat(names," "))
+    table.remove(names,index)
+  end
+  assert={
+    ["are"]={
+      ["same"]=function()
+      end
+    }
+  }
+end
+if not help then
+  require "help"
+end
+describe("Help",function()
+  it("should return {_(\"Please see http://iggyvolz.github.io/iMagine/documentation.html for help\"),_(\"NOTE: still in development so this documentation is likely irrelevant\")}",function()
+    assert.are.same({_("Please see http://iggyvolz.github.io/iMagine/documentation.html for help"),_("NOTE: still in development so this documentation is likely irrelevant")},help)
+  end)
+end)
