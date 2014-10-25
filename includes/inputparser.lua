3ac6273f5b4d5a322be71a0f0fce31ef50725984
iggyvolz <iggyvolz@aim.com>
2014-10-25 15:40:48 -0400

    Revamp InputParser

--- includes/inputparser.lua
+++ includes/inputparser.lua
@@ -1,55 +1,39 @@
 -- Temporary default subject - reprogram later
 defaultSubject="tony"
+function string:split(div) -- credit: http://richard.warburton.it
+  if (div=='') then return false end
+  local pos,arr = 0,{}
+  for st,sp in function() return string.find(self,div,pos,true) end do
+    table.insert(arr,string.sub(self,pos,st-1))
+    pos = sp + 1
+  end
+  table.insert(arr,string.sub(self,pos))
+  return arr
+end
 function inputParser (text)
    local otext=text
-   local stage=1
    local subject=""
    local action=""
    local params={}
-   local continue=0
-   local char=""
+   text=text:lower()
    if not text:find(".",1,1) then
       text="i."..text
    end
-   for i=1,#text do
-      continue=0
-      char=text:sub(i,i)
-      if stage==1 then
-         if char=="." then
-            stage=2
-            continue=1
-            if subject=="i" then
-               subject=defaultSubject
-            end
-         else
-            subject=subject..char
-         end
-      end
-      if stage==2 and continue~=1 then
-         if char=="(" then
-            stage=3
-            if #text==i then
-              break
-            end
-            if text:sub(i+1,i+1) == ")" then
-              break
-            end
-            table.insert(params,"")
-            continue=1
-         else
-            action=action..char
-         end
-      end
-      if stage==3 and continue~=1 then
-         if char==")" then
-            break
-         end
-         if char=="," then
-            table.insert(params,"")
-         else
-            params[#params]=params[#params]..char
-         end
-      end
+   if not text:find("(",1,1) then
+     text=text.."()"
    end
-   return text,subject,action,params
+   if not text:find(")",1,1) then
+     text=text..(")")
+   end
+   subject=text:split(".")[1]
+   if subject == "i" then
+     subject=defaultSubject
+   end
+   action=text:split(".")[2]:split("(")[1]
+   if #text:split(".")[2]:split("(")[2]:split(")")[1] == 0 then
+     params={}
+   else
+     params=text:split(".")[2]:split("(")[2]:split(")")[1]:split(",")
+   end
+   return otext,subject,action,params
 end
